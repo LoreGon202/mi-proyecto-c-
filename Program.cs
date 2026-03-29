27404b48121555e0259f9ad51642f8a17901e0c8
lorenacorrea2022@hotmail.com <lorenacorrea2022@hotmail.com>
2026-03-28 21:28:43 -0500

    primer commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+           var Name = "Jhon";
+           Name = "1Jhon";
+
+            Console.WriteLine($"Hello world! {Name}");
+            Console.WriteLine();
+        }
+    }
+}
