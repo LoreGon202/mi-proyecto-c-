2da910e0967fe735d2e1d26046c2a49455d71287
lorenacorrea2022@hotmail.com <lorenacorrea2022@hotmail.com>
2026-04-04 17:39:09 -0500

    actualizando codigo

--- Program.cs
+++ Program.cs
@@ -6,12 +6,45 @@ namespace HelloWorld
     {
         static void Main()
         {
-            var Name = "Jhon Smith";
+            DateOnly dateConverted;
+            string nameInput;
+            string birthdayInput;
 
-            Console.WriteLine($"Hello world! {Name}");
-            Console.WriteLine( $"Thanks for ccoming {Name}"); // AQUÍ
-            Console.WriteLine();
-            Console.WriteLine("PRUEBA GIT"); // AQUÍS
+            Console.WriteLine("Hola bienvenido al calculador de años");
+            Console.Write("Escribe tu nombre: ");
+            nameInput = Console.ReadLine() ?? "";
+
+            Console.WriteLine($"Un gusto conocerte {nameInput}");
+            Console.Write("Escribe tu fecha de nacimiento en formato dd/mm/yyyy: ");
+            birthdayInput = Console.ReadLine() ?? "";
+
+            bool isDateValid = DateOnly.TryParse(birthdayInput, out dateConverted);
+
+            if (!isDateValid)
+            {
+                Console.WriteLine($"La fecha de nacimiento es inválida: {birthdayInput}");
+                return;
+            }
+
+            var person = new Person
+            {
+                Name = nameInput,
+                Birthday = dateConverted,
+                Age = DateTime.Now.Year - dateConverted.Year
+            };
+
+            Console.WriteLine($"Tu nombre: {person.Name}");
+            Console.WriteLine($"Tu fecha de nacimiento: {person.Birthday}");
+            Console.WriteLine($"Tu edad es {person.Age} años");
+
+            Console.ReadLine();
         }
+    }
+
+    class Person
+    {
+        public string Name { get; set; } = "";
+        public DateOnly Birthday { get; set; }
+        public int Age { get; set; }
     }
 }
