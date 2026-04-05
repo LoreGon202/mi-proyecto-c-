15025a4bca9c90d5e42f50306f13d7d6181e183f
lorenacorrea2022@hotmail.com <lorenacorrea2022@hotmail.com>
2026-04-04 19:35:50 -0500

    agrego carpeta

--- conditionals_and_operators/Program.cs
+++ conditionals_and_operators/Program.cs
@@ -1,1 +1,12 @@
-hola 
+﻿// string nameInput= string.empty;
+Console.WriteLine("Ingrese un número: ");
+string? input = Console.ReadLine();
+if (string.IsNullOrEmpty(input) || !int.TryParse(input, out int num))
+{
+    Console.WriteLine("Entrada inválida. Por favor ingrese un número entero.");
+    return;
+}
+
+if (num > 0) Console.WriteLine("Positivo");
+else if (num < 0) Console.WriteLine("Negativo");
+else Console.WriteLine("Es cero");
